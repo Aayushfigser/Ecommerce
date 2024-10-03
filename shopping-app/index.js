6bf7dbf1685e100bf6471d7a583f6f6b4e6ccacd
RoshanZameerJ <121081733+RoshanZameerJ@users.noreply.github.com>
2024-10-03 18:56:58 +0530

    origin added

--- shopping-app/index.js
+++ shopping-app/index.js
@@ -6,7 +6,9 @@ const { connectProducer } = require('./kafka/Producer');
 const cors = require('cors');
 const path = require('path');
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // Connect to MongoDB
@@ -24,7 +26,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
