3e28b30cd6a87a42848b005a860f08a01504800a
RoshanZameerJ <121081733+RoshanZameerJ@users.noreply.github.com>
2024-10-03 19:48:50 +0530

    healt

--- shopping-app/index.js
+++ shopping-app/index.js
@@ -22,6 +22,9 @@ connectDB();
 app.use('/api/v1.0/shopping', require('./routes/userRoutes'));
 app.use('/api/v1.0/shopping', require('./routes/productRoutes'));
 app.use('/api/v1.0/shopping', require('./routes/orderRoutes'));
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
